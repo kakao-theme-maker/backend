30da94748580a37a2dd18f16ce4d42f058a6b74a
kym8821 <louie9798@naver.com>
2025-04-08 21:00:09 +0900

    [test] 토큰 관리 객체의 테스트 코드 작성 (JwtUtilsTest)

---
+++ common/src/test/java/com/theme/auth/JwtUtilsTest.java
@@ -0,0 +1,69 @@
+package com.theme.auth;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class JwtUtilsTest {
+    @Mock
+    private JwtUtils jwtUtils;
+
+    String secretKey = "TestSecretKey1234TestSecretKey1234TestSecretKey1234TestSecretKey1234TestSecretKey1234";
+
+    @BeforeEach
+    void setUp() {
+        jwtUtils = new JwtUtils(secretKey);
+    }
+
+    @Test
+    void validateToken_test() {
+        // given
+        String userEmail = "test@test.com";
+        String accessToken = jwtUtils.generateAccessToken(userEmail);
+        String wrongToken = "wrongToken";
+        // when + then
+        assert jwtUtils.validateToken(accessToken);
+        assert !jwtUtils.validateToken(wrongToken);
+    }
+
+    @Test
+    void generateAccessToken_test(){
+        // given
+        String userEmail = "test@test.com";
+        // when
+        String accessToken = jwtUtils.generateAccessToken(userEmail);
+        // then
+        assertNotNull(accessToken);
+    }
+
+    @Test
+    void generateRefreshToken_test(){
+        // given
+        String userEmail = "test@test.com";
+        // when
+        String refreshToken = jwtUtils.generateRefreshToken(userEmail);
+        // then
+        assertNotNull(refreshToken);
+    }
+
+    @Test
+    void getEmail_test(){
+        // given
+        String userEmail = "test@test.com";
+        String ghostEmail = "ghost@test.com";
+        String accessToken = jwtUtils.generateAccessToken(userEmail);
+        String wrongToken = "wrongToken";
+        // when
+        String savedEmail = jwtUtils.getEmail(accessToken);
+        String mustBeNull = jwtUtils.getEmail(wrongToken);
+        // then
+        assert !savedEmail.equals(ghostEmail);
+        assert savedEmail.equals(userEmail);
+        assert mustBeNull == null;
+    }
+}
