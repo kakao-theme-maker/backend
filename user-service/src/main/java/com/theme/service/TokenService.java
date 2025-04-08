fbac6f86803736cb5d01a6e9b42204b0748d9314
kym8821 <louie9798@naver.com>
2025-04-08 20:49:02 +0900

    [feat] redis 설정 추가 및 저장소 접근 기능 추가 - RedisTemplate, RedisConnectionFactory 설정용 빈 추가 (RedisConfig) - Redis 데이터 접근용 빈 추가 (RedisSingleDataService) - 인증 및 인가 토큰 접근용 빈 추가 (TokenService)

---
+++ user-service/src/main/java/com/theme/service/TokenService.java
@@ -0,0 +1,52 @@
+package com.theme.service;
+
+import com.theme.constants.AuthProperty;
+import com.theme.redis.RedisSingleDataService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+    private final RedisSingleDataService redisSingleDataService;
+
+    public TokenService(RedisSingleDataService redisSingleDataService) {
+        this.redisSingleDataService = redisSingleDataService;
+    }
+
+    public String getAccessTokenKey(String email){
+        return "access_token_" + email;
+    }
+
+    public String getRefreshTokenKey(String email){
+        return "refresh_token_" + email;
+    }
+
+    public boolean saveAccessToken(String email, String accessToken) {
+        return redisSingleDataService.set(getAccessTokenKey(email), accessToken, AuthProperty.ACCESS_TOKEN_EXPIRES_IN.intValue());
+    }
+
+    public boolean saveRefreshToken(String email, String refreshToken) {
+        return redisSingleDataService.set(getRefreshTokenKey(email), refreshToken, AuthProperty.REFRESH_TOKEN_EXPIRES_IN.intValue());
+    }
+
+    public boolean saveAccessAndRefreshToken(String email, String accessToken, String refreshToken) {
+        boolean success1 = saveAccessToken(email, accessToken);
+        boolean success2 = saveRefreshToken(email, refreshToken);
+        return success1 && success2;
+    }
+
+    public String getAccessToken(String email) {
+        return redisSingleDataService.get(getAccessTokenKey(email));
+    }
+
+    public String getRefreshToken(String email){
+        return redisSingleDataService.get(getRefreshTokenKey(email));
+    }
+
+    public boolean deleteAccessToken(String email) {
+        return redisSingleDataService.delete(getAccessTokenKey(email));
+    }
+
+    public boolean deleteRefreshToken(String email) {
+        return redisSingleDataService.delete(getRefreshTokenKey(email));
+    }
+}
