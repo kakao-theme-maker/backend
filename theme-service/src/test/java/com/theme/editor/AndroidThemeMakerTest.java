a81ed21d4d7d4084676857d92901edea2d42ef4e
kym8821 <louie9798@naver.com>
2025-05-08 21:00:41 +0900

    [test] 커스텀 테마 생성 테스트 코드 추가

---
+++ theme-service/src/test/java/com/theme/editor/AndroidThemeMakerTest.java
@@ -0,0 +1,34 @@
+package com.theme.editor;
+
+import com.theme.domain.ThemeComponent;
+import com.theme.repository.ThemeComponentRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AndroidThemeMakerTest {
+    @Autowired
+    AndroidThemeMaker androidThemeMaker;
+
+    @Autowired
+    ThemeComponentRepository themeComponentRepository;
+
+    @Test
+    void makeTheme() {
+        // given
+        themeComponentRepository.save(ThemeComponent.builder()
+                .themeName("test theme")
+                .userEmail("test@test.com")
+                .isDone(true)
+                .versionNumber("0.0.0")
+                .versionName("test version")
+                .build());
+        // when & then
+        assertDoesNotThrow(() -> androidThemeMaker.makeTheme(1));
+    }
+}
