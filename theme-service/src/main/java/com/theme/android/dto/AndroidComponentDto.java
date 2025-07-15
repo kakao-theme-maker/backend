9fe790a3229c1680de4627609160a4f11bb010c0
kym8821 <louie9798@naver.com>
2025-07-15 21:26:01 +0900

    [refactor] theme 관련 도메인을 기준으로 코드 분리

---
+++ theme-service/src/main/java/com/theme/android/dto/AndroidComponentDto.java
@@ -0,0 +1,32 @@
+package com.theme.android.dto;
+
+import com.theme.domain.ComponentType;
+import com.theme.domain.DesignComponent;
+import com.theme.theme.domain.ThemeImage;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AndroidComponentDto {
+    String imageUrl;
+    String AndroidComponentPath;
+    String AndroidComponentName;
+    Integer sizeX;
+    Integer sizeY;
+
+    public static AndroidComponentDto fromEntity(ThemeImage themeImage) {
+        DesignComponent component = themeImage.getDesignComponent();
+        ComponentType componentType = component.getComponentType();
+        return AndroidComponentDto.builder()
+                .imageUrl(component.getImageUrl())
+                .AndroidComponentName(componentType.getAndroidComponentName())
+                .AndroidComponentPath(componentType.getAndroidComponentPath())
+                .sizeX(componentType.getSizeX())
+                .sizeY(componentType.getSizeY()).build();
+    }
+}
