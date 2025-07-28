0590122bba5df011e3d54800566d6c8ba87a6b23
kym8821 <louie9798@naver.com>
2025-07-28 21:58:13 +0900

    [feat] 게시글 관련 라우팅 정보 추가

--- gateway-service/src/main/java/gateway/com/RouteLocatorConfig.java
+++ gateway-service/src/main/java/gateway/com/RouteLocatorConfig.java
@@ -7,18 +7,19 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RouteLocatorConfig {
-    private static final String USER_SERVICE_URL = "lb://USER-SERVICE";
-    private static final String THEME_SERVICE_URL = "lb://THEME-SERVICE";
 
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("user-service", r -> r
-                        .path("/users/**")
-                        .uri(USER_SERVICE_URL))
-                .route("theme-service", r -> r
-                        .path("/api/**")
-                        .uri(THEME_SERVICE_URL))
-                .build();
-    }
+  private static final String USER_SERVICE_URL = "lb://USER-SERVICE";
+  private static final String THEME_SERVICE_URL = "lb://THEME-SERVICE";
+
+  @Bean
+  public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    return builder.routes()
+        .route("user-service", r -> r
+            .path("/users/**", "/posts/**")
+            .uri(USER_SERVICE_URL))
+        .route("theme-service", r -> r
+            .path("/api/**")
+            .uri(THEME_SERVICE_URL))
+        .build();
+  }
 }
