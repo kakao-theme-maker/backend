59aa3dae02bbadddcff247f7a71c76fdd3949d23
kym8821 <louie9798@naver.com>
2025-07-28 21:51:55 +0900

    [feat] 게시판 관련 entity 생성 - post(게시글) - comment(댓글) - prefer(좋아요) - tag(태그)

--- user-service/src/main/java/com/theme/domain/User.java
+++ user-service/src/main/java/com/theme/domain/User.java
@@ -1,13 +1,23 @@
 package com.theme.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import com.theme.post.domain.Prefer;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -16,19 +26,23 @@ import java.time.LocalDateTime;
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 public class User {
-    @Id
-    @Column(unique = true, nullable = false)
-    String userEmail;
-    @Column
-    Gender gender;
-    @Column
-    LocalDate birth;
-    @Column
-    String profileImg;
-    @Column
-    String introduce;
-    @CreatedDate
-    LocalDateTime createdAt;
-    @LastModifiedDate
-    LocalDateTime updatedAt;
+
+  @Id
+  @Column(unique = true, nullable = false)
+  String userEmail;
+  @Column
+  Gender gender;
+  @Column
+  LocalDate birth;
+  @Column
+  String profileImg;
+  @Column
+  String introduce;
+  @CreatedDate
+  LocalDateTime createdAt;
+  @LastModifiedDate
+  LocalDateTime updatedAt;
+
+  @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.ALL)
+  List<Prefer> prefers;
 }
---
+++ user-service/src/main/java/com/theme/post/domain/Tag.java
@@ -0,0 +1,43 @@
+package com.theme.post.domain;
+
+import com.theme.post.dto.TagDto.TagUpdateDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long tagId;
+
+  @Column
+  private String tagName;
+
+  @ManyToOne
+  private Post post;
+
+  public static Tag createTransient(String tagName, Post post) {
+    return Tag.builder()
+        .tagName(tagName)
+        .post(post).build();
+  }
+
+  public void update(TagUpdateDto updateDto) {
+    if (tagName != null) {
+      this.tagName = updateDto.getTagName();
+    }
+  }
+}
