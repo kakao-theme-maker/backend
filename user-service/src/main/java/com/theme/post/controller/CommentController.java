5fa13b8fa9c0465558fcb1669f1d4f2cbfe30a2d
kym8821 <louie9798@naver.com>
2025-07-28 21:53:19 +0900

    [feat] Comment CRUD와 DTO 추가 - 게시글의 댓글 전체조회, 댓글정보 조회, 댓글 생성, 수정, 삭제 기능 조회 - 응답, 생성, 수정용 댓글 dto 생성

---
+++ user-service/src/main/java/com/theme/post/controller/CommentController.java
@@ -0,0 +1,60 @@
+package com.theme.post.controller;
+
+import com.theme.post.dto.CommentDto.CommentCreateDto;
+import com.theme.post.dto.CommentDto.CommentResponse;
+import com.theme.post.dto.CommentDto.CommentUpdateDto;
+import com.theme.post.service.CommentService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+public class CommentController {
+
+  private final CommentService commentService;
+
+  @GetMapping("/{postId}/comments")
+  public ResponseEntity<List<CommentResponse>> getComments(@PathVariable Long postId,
+      @RequestParam("pageNumber") Integer pageNumber, @RequestParam("pageSize") Integer pageSize) {
+    pageNumber = pageNumber == null ? 0 : pageNumber;
+    pageSize = pageSize == null ? 20 : pageSize;
+    return ResponseEntity.ok(
+        commentService.getComments(postId, pageNumber, pageSize).stream().map(CommentResponse::from)
+            .toList());
+  }
+
+  @GetMapping("/comments/{commentId}")
+  public ResponseEntity<CommentResponse> getComment(@PathVariable Long commentId) {
+    return ResponseEntity.ok(CommentResponse.from(commentService.getCommentById(commentId)));
+  }
+
+  @PostMapping("/comments")
+  public ResponseEntity<CommentResponse> createComment(
+      @RequestBody CommentCreateDto createDto) {
+    return ResponseEntity.ok(CommentResponse.from(commentService.saveComment(createDto)));
+  }
+
+  @PutMapping("/comments/{commentId}")
+  public ResponseEntity<CommentResponse> updateComment(@PathVariable Long commentId,
+      @RequestBody CommentUpdateDto updateDto) {
+    return ResponseEntity.ok(
+        CommentResponse.from(commentService.updateComment(commentId, updateDto)));
+  }
+
+  @DeleteMapping("/comments/{commentId}")
+  public ResponseEntity<CommentResponse> deleteComment(@PathVariable Long commentId) {
+    commentService.deleteComment(commentId);
+    return ResponseEntity.noContent().build();
+  }
+}
