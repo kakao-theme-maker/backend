75245cc43f9d9166ffe14f55af8f0933e9f9c3d4
kym8821 <louie9798@naver.com>
2025-09-12 20:41:20 +0900

    feat : FileManager 인터페이스 생성 및 S3FileManaer에 적용

--- src/main/java/com/komentum/global/utils/S3FileManager.java
+++ src/main/java/com/komentum/global/utils/S3FileManager.java
@@ -1,5 +1,6 @@
-package com.komentum.theme.utils;
+package com.komentum.global.utils;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -16,18 +17,20 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Component
 @Profile("!test")
-public class S3FileManager {
+public class S3FileManager implements FileManager {
 
   private final S3Client s3Client;
   private final String cloudFront;
+  private final String bucketName;
 
   public S3FileManager(@Value("${aws.s3.access-key}") String accessKey,
       @Value("${aws.s3.secret-key}") String secretKey, @Value("${aws.s3.region}") String region,
-      @Value("${aws.s3.cloudfront}") String cloudFront) {
-    if (cloudFront == null || cloudFront.trim().isEmpty()) {
-      throw new IllegalArgumentException("[S3 File Manager] cloudFront is null or empty");
-    }
-    this.cloudFront = cloudFront;
+      @Value("${aws.s3.cloudfront}") String cloudFront,
+      @Value("{aws.s3.theme-bucket-name}") String bucketName) {
+    this.cloudFront = Optional.ofNullable(cloudFront)
+        .orElseThrow(() -> new IllegalArgumentException("cloudFront is null"));
+    this.bucketName = Optional.ofNullable(bucketName)
+        .orElseThrow(() -> new IllegalArgumentException("bucket is null"));
     s3Client = S3Client.builder()
         .region(Region.of(region))
         .credentialsProvider(
@@ -35,7 +38,7 @@ public class S3FileManager {
         .build();
   }
 
-  public String resolveFilePath(String fileName) {
+  private String resolveFilePath(String fileName) {
     if (fileName == null || fileName.trim().isEmpty()) {
       throw new IllegalArgumentException("[S3 File Manager] fileName is null or empty");
     }
@@ -45,11 +48,10 @@ public class S3FileManager {
   /**
    * upload a file to s3 bucket
    *
-   * @param fileBytes  file byte array
-   * @param fileName   name of the file
-   * @param bucketName name of the bucket to upload the file to
+   * @param fileBytes file bytes array
+   * @param fileName  name of the file
    */
-  public String uploadFile(byte[] fileBytes, String fileName, String bucketName) {
+  public String uploadFile(byte[] fileBytes, String fileName) {
     PutObjectRequest putObjectRequest = PutObjectRequest.builder()
         .bucket(bucketName)
         .key(fileName)
@@ -62,10 +64,9 @@ public class S3FileManager {
   /**
    * delete a file from s3 bucket
    *
-   * @param fileName   name of the file
-   * @param bucketName name of the bucket to delete the file from
+   * @param fileName name of the file
    */
-  public void deleteFile(String fileName, String bucketName) {
+  public void deleteFile(String fileName) {
     DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
         .bucket(bucketName)
         .key(fileName)
@@ -76,10 +77,9 @@ public class S3FileManager {
   /**
    * get a file from s3 bucket
    *
-   * @param fileName   name of the file
-   * @param bucketName name of the bucket to get the file from
+   * @param fileName name of the file
    */
-  public byte[] downloadFile(String fileName, String bucketName) {
+  public byte[] downloadFile(String fileName) {
     GetObjectRequest getObjectRequest = GetObjectRequest.builder()
         .bucket(bucketName)
         .key(fileName)
