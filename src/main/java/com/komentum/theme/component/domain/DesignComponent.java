b4b4a29e25306b11f000404a4bfdf9fee8984756
klaqwe1 <klaqwe1@naver.com>
2025-09-16 00:23:57 +0900

    fix: remove componentType

--- src/main/java/com/komentum/theme/component/domain/DesignComponent.java
+++ src/main/java/com/komentum/theme/component/domain/DesignComponent.java
@@ -2,12 +2,9 @@ package com.komentum.theme.component.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -32,10 +29,6 @@ public class DesignComponent {
   private Integer designComponentId;
 
   private String userEmail;
-
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "component_type_id")
-  private ComponentType componentType; // 연관 관계 설정
 
   private String imageUrl;
 
