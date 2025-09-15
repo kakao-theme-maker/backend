637569d6499e841467b57bbd4524273eefcddfc8
klaqwe1 <klaqwe1@naver.com>
2025-09-16 00:26:00 +0900

    fix: remove componentType

--- src/main/java/com/komentum/theme/component/dto/DesignComponentDto.java
+++ src/main/java/com/komentum/theme/component/dto/DesignComponentDto.java
@@ -14,25 +14,8 @@ public class DesignComponentDto {
 
   private Integer designComponentId;
   private String userEmail;
-  private ComponentTypeDto componentType;
   private String imageUrl;
   private LocalDateTime createdAt;
   private LocalDateTime updatedAt;
   private Boolean isPublic;
-
-  @Data
-  @Builder
-  @NoArgsConstructor
-  @AllArgsConstructor
-  public static class ComponentTypeDto {
-
-    private Integer componentTypeId;
-    private String explain;
-    private String iosComponentPath;
-    private String iosComponentName;
-    private String androidComponentPath;
-    private String androidComponentName;
-    private Integer sizeX;
-    private Integer sizeY;
-  }
 }
