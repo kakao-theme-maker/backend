6ba6263d6b3328ec431d6e238f61c32dc413736e
klaqwe1 <klaqwe1@naver.com>
2025-09-18 12:21:02 +0900

    fix: remove invalid code

--- src/main/java/com/komentum/theme/theme/repository/ThemeStyleRepository.java
+++ src/main/java/com/komentum/theme/theme/repository/ThemeStyleRepository.java
@@ -1,30 +1,22 @@
 package com.komentum.theme.theme.repository;
 
 import com.komentum.theme.theme.domain.ThemeStyle;
-import com.komentum.theme.theme.domain.ThemeStyleId;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface ThemeStyleRepository extends JpaRepository<ThemeStyle, ThemeStyleId> {
+public interface ThemeStyleRepository extends JpaRepository<ThemeStyle, Integer> {
 
   @Query("select ts from ThemeStyle ts join fetch ThemeComponent tc where tc.themeComponentId = :themeComponentId")
   List<ThemeStyle> findByThemeComponentId(
-      @Param("themeComponentId") Integer themeComponentId); // component 리스트 반환
+          @Param("themeComponentId") Integer themeComponentId); // component 리스트 반환
 
   @Modifying
   @Query("DELETE FROM ThemeStyle ts WHERE ts.themeComponent.themeComponentId = :themeComponentId")
   void deleteByThemeComponentId(
-      @Param("themeComponentId") Integer themeComponentId); // component 삭제
-
-  @Modifying
-  @Transactional
-  @Query("DELETE FROM ThemeStyle ts WHERE ts.themeComponentId = :themeComponentId AND ts.cssSelector IS NOT NULL")
-  void deleteByThemeComponentIdAndCssSelectorIsNotNull(
-      @Param("themeComponentId") Integer themeComponentId); // CSS 커스터마이징 데이터만 삭제
+          @Param("themeComponentId") Integer themeComponentId); // component 삭제
 }
