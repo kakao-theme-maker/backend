403d303048dfcd0a827b6699ef92bda4312f374a
kym8821 <louie9798@naver.com>
2025-11-18 23:15:59 +0900

    feat : [#31 - ]FileManager 인터페이스 행위 추가
    FileManager 인터페이스에 이미지 경로 생성 행위 추가
    S3FileManager의 resolveFilePath 접근자 Public로 변경
    
    Fixes: #31

--- src/main/java/com/komentum/global/utils/FileManager.java
+++ src/main/java/com/komentum/global/utils/FileManager.java
@@ -2,6 +2,8 @@ package com.komentum.global.utils;
 
 public interface FileManager {
 
+  String resolveFilePath(String fileName);
+
   String uploadFile(byte[] fileBytes, String fileName);
 
   void deleteFile(String fileName);
