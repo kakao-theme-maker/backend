fd9487d218bc737ccbd136f93a6b0822079004b0
klaqwe1 <klaqwe1@naver.com>
2025-11-22 01:23:25 +0900

    fix: Redis template set 오버로드 문제 해결 second가 offset 처리 되어 null padding이 발생한 문제 해결 Related to #30

--- src/main/java/com/komentum/user/redis/RedisSingleDataService.java
+++ src/main/java/com/komentum/user/redis/RedisSingleDataService.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @Component
 public class RedisSingleDataService {
@@ -20,7 +22,7 @@ public class RedisSingleDataService {
 
   public boolean set(String key, String value, int seconds) {
     try {
-      redisTemplate.opsForValue().set(key, value, seconds);
+      redisTemplate.opsForValue().set(key, value, seconds, TimeUnit.SECONDS);
       return true;
     } catch (Exception e) {
       log.error(e.getMessage());
