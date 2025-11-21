72db29242352e187099d7c81992fd48ea267478e
kym8821 <louie9798@naver.com>
2025-11-22 00:52:05 +0900

    test: [#31 - ]테스트 코드에 content tye 추가
    ThemeManageControllerTest에서 요청 시 content-type: Application_json 추가

--- src/test/java/com/komentum/theme/theme/controller/ThemeManageControllerTest.java
+++ src/test/java/com/komentum/theme/theme/controller/ThemeManageControllerTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -68,6 +69,7 @@ class ThemeManageControllerTest {
         .build();
     // when
     MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/api/themes")
+        .contentType(MediaType.APPLICATION_JSON)
         .content(objectMapper.writeValueAsString(createThemeRequest));
     request = mockMvcUtils.generateAuthJsonRequest(request, themeDataGenerator.userEmail);
     // then
@@ -91,6 +93,7 @@ class ThemeManageControllerTest {
     // when
     MockHttpServletRequestBuilder request = MockMvcRequestBuilders.put("/api/themes/{id}",
             target.getThemeComponentId())
+        .contentType(MediaType.APPLICATION_JSON)
         .content(objectMapper.writeValueAsString(createThemeRequest));
     request = mockMvcUtils.generateAuthJsonRequest(request, themeDataGenerator.userEmail);
     // then
