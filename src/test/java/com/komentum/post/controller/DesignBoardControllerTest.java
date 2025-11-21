27721898d24146b9ed21e10e7da4db487fa861ea
kym8821 <louie9798@naver.com>
2025-11-21 23:41:25 +0900

    test: [#31 - ] DesignBoard 수정, 삭제 테스트 작성 및 assertion 보완
    DesignBoardDetailDTO에 대한 assertion 시 DB에 있는 값도 확인하도록 개선 및 공통 기능으로 분리
    ThemeBoardDataGenerator 삭제
    FileManager 전역 모킹 시 인터페이스를 기반으로 하도록 개선
    
    Fixes: #31

--- src/test/java/com/komentum/post/controller/DesignBoardControllerTest.java
+++ src/test/java/com/komentum/post/controller/DesignBoardControllerTest.java
@@ -8,12 +8,16 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.komentum.config.EnableTestProfile;
 import com.komentum.global.utils.FileManager;
 import com.komentum.post.domain.DesignBoard;
+import com.komentum.post.domain.Post;
 import com.komentum.post.dto.DesignBoardDto.DesignBoardCreateDto;
 import com.komentum.post.dto.DesignBoardDto.DesignBoardDetailDto;
 import com.komentum.post.dto.DesignBoardDto.DesignBoardPreviewDto;
+import com.komentum.post.dto.DesignBoardDto.DesignBoardUpdateDto;
 import com.komentum.post.dto.TagDto.TagCreateDto;
 import com.komentum.post.dto.TagDto.TagResponse;
+import com.komentum.post.dto.TagDto.TagUpdateDto;
 import com.komentum.post.repository.DesignBoardRepository;
+import com.komentum.post.repository.PostRepository;
 import com.komentum.test.BoardDetailDataGenerator;
 import com.komentum.test.MockMvcUtils;
 import com.komentum.theme.component.domain.DesignComponent;
@@ -48,6 +52,9 @@ public class DesignBoardControllerTest {
   private BoardDetailDataGenerator boardDetailDataGenerator;
 
   @Autowired
+  private PostRepository postRepository;
+
+  @Autowired
   private DesignBoardRepository designBoardRepository;
 
   @Autowired
@@ -65,6 +72,36 @@ public class DesignBoardControllerTest {
   @AfterEach
   public void tearDown() {
     boardDetailDataGenerator.deleteDesignBoards();
+  }
+
+  private void assertDesignBoard(DesignBoardDetailDto response, String expectedTitle,
+      String expectedContent) {
+    // field assertion
+    assertThat(response)
+        .isNotNull();
+    assertThat(response.getTitle())
+        .isEqualTo(expectedTitle);
+    assertThat(response.getContent())
+        .isEqualTo(expectedContent);
+    // DB assertion
+    DesignBoard savedData = designBoardRepository.findByPost_PostId(response.getBoardId())
+        .orElse(null);
+    assertThat(savedData)
+        .isNotNull();
+    Post post = savedData.getPost();
+    assertThat(post.getTitle())
+        .isEqualTo(expectedTitle);
+    assertThat(post.getContent())
+        .isEqualTo(expectedContent);
+  }
+
+  private void assertDesignBoard(DesignBoardDetailDto response, String expectedTitle,
+      String expectedContent, List<String> expectedTags) {
+    // field assertion
+    assertThat(response.getTags()).extracting(TagResponse::getTagName)
+        .containsExactlyElementsOf(expectedTags);
+    // field and DB assertion
+    assertDesignBoard(response, expectedTitle, expectedContent);
   }
 
   @Test
@@ -91,18 +128,16 @@ public class DesignBoardControllerTest {
   public void whenSendRequest_retrieveDesignBoardById() throws Exception {
     // given
     DesignBoard targetDesignBoard = boardDetailDataGenerator.getDesignBoards().get(0);
+    Post targetPost = targetDesignBoard.getPost();
     String requestPath = String.format("/api/design-boards/%d",
-        targetDesignBoard.getDesignBoardId());
+        targetDesignBoard.getPost().getPostId());
     User client = boardDetailDataGenerator.getUsers().get(0);
     // when
     DesignBoardDetailDto response = mockMvcUtils.requestGet(mockMvc, requestPath, null,
         client.getUserEmail(), new TypeReference<>() {
         });
     // then
-    assertThat(response.getBoardId())
-        .isEqualTo(targetDesignBoard.getDesignBoardId());
-    assertThat((long) response.getDesignComponentId())
-        .isEqualTo(targetDesignBoard.getDesignBoardId());
+    assertDesignBoard(response, targetPost.getTitle(), targetPost.getContent());
   }
 
   @Test
@@ -137,12 +172,54 @@ public class DesignBoardControllerTest {
     DesignBoardDetailDto response = mockMvcUtils.performMultipartRequest(mockMvc, requestPath,
         HttpMethod.POST, null, author.getUserEmail(), formDataList, new TypeReference<>() {
         });
+    // then : 필드 및 DB 검증
+    assertDesignBoard(response, createDto.getTitle(), createDto.getContent(), tags);
+  }
+
+  @Test
+  @DisplayName("when send request, update design board info")
+  public void whenSendRequest_updateDesignBoard() throws Exception {
+    // given
+    DesignBoard targetDesignBoard = boardDetailDataGenerator.getDesignBoards().get(0);
+    String requestPath = String.format("/api/design-boards/%d",
+        targetDesignBoard.getPost().getPostId());
+    User author = targetDesignBoard.getPost().getUser();
+    String expectedTitle = UUID.randomUUID().toString();
+    String expectedContent = UUID.randomUUID().toString();
+    List<String> expectedTags = IntStream.range(0, 5).mapToObj(i -> UUID.randomUUID().toString())
+        .toList();
+    DesignBoardUpdateDto updateDto = DesignBoardUpdateDto.builder()
+        .title(expectedTitle)
+        .content(expectedContent)
+        .postTags(
+            expectedTags.stream().map(t -> TagUpdateDto.builder().tagName(t).build()).toList())
+        .userEmail(author.getUserEmail())
+        .publicFlag(false)
+        .build();
+    // when
+    DesignBoardDetailDto response = mockMvcUtils.requestPut(mockMvc, requestPath, null,
+        author.getUserEmail(), updateDto, new TypeReference<>() {
+        });
+    // then : 필드 및 DB 검증
+    assertDesignBoard(response, expectedTitle, expectedContent, expectedTags);
+  }
+
+  @Test
+  @DisplayName("when send request, delete board info")
+  public void whenSendRequest_deleteDesignBoard() throws Exception {
+    // given
+    DesignBoard targetDesignBoard = boardDetailDataGenerator.getDesignBoards().get(0);
+    User author = targetDesignBoard.getPost().getUser();
+    String requestPath = String.format("/api/design-boards/%d",
+        targetDesignBoard.getPost().getPostId());
+    // when
+    mockMvcUtils.requestDelete(mockMvc, requestPath, null, author.getUserEmail(), null,
+        new TypeReference<Void>() {
+        });
     // then
-    assertThat(response.getProfileImageUrl()).isEqualTo(expectedProfileImageUrl);
-    assertThat(response.getDesignComponentId()).isEqualTo(
-        (long) unsavedBoardDesignComponent.getDesignComponentId());
-    assertThat(response.getTags())
-        .extracting(TagResponse::getTagName)
-        .containsExactlyInAnyOrder(tags.toArray(new String[]{}));
+    assertThat(designBoardRepository.findById(targetDesignBoard.getDesignBoardId()))
+        .isEmpty();
+    assertThat(postRepository.findById(targetDesignBoard.getPost().getPostId()))
+        .isEmpty();
   }
 }
