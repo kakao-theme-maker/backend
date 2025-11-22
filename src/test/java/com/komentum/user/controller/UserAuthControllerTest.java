7595038363d7207efc2656e57888519d985be8d5
klaqwe1 <klaqwe1@naver.com>
2025-11-23 03:25:57 +0900

    Test: 로컬 회원가입, 로그인 테스트 코드 Related to: #30

--- src/test/java/com/komentum/user/controller/UserAuthControllerTest.java
+++ src/test/java/com/komentum/user/controller/UserAuthControllerTest.java
@@ -1,4 +1,108 @@
+package com.komentum.user.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.komentum.auth.JwtUtils;
+import com.komentum.config.EnableTestProfile;
+import com.komentum.test.UserDataGenerator;
+import com.komentum.user.dto.LocalLoginRequestDto;
+import com.komentum.user.dto.UserAuthResponse;
+import com.komentum.user.repository.UserRepository;
+import com.komentum.user.service.UserAuthService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@EnableTestProfile
+@AutoConfigureMockMvc
+@SpringBootTest
 class UserAuthControllerTest {
-  
+    String email = "admin1@gmail.com";
+    String password ="qwer123!";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserDataGenerator userDataGenerator;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @BeforeEach
+    void setUp(){
+        userDataGenerator.deleteAllUsers();
+        userDataGenerator.generateTestLocalUser(email, password);
+    }
+
+    @AfterEach
+    void tearDown(){
+        userDataGenerator.deleteAllUsers();
+    }
+
+    @Autowired
+    private UserAuthController userAuthController;
+
+    @Test
+    @DisplayName("회원가입")
+    void signUpWithLocalTest() throws Exception{
+        //given
+        String signUpEmail = "admin1@gmail.com";
+        String signUpPassword ="qwer123!";
+        LocalLoginRequestDto localLoginRequestDto
+                = LocalLoginRequestDto.builder()
+                .email(signUpEmail)
+                        .password(signUpPassword)
+                                .build();
+        //when
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/api/auth/local/sign-up")
+                .content(objectMapper.writeValueAsString(localLoginRequestDto))
+                .contentType(MediaType.APPLICATION_JSON); //웬만하면 하는 게 좋음
+        //then
+        mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
+                .andReturn().getResponse().getContentAsString();
+        assertThat(userRepository.findByUserEmail(email)).isPresent();
+    }
+
+    @Test
+    @DisplayName("로그인 jwt 토큰 검증")
+    void signInWithLocalTest() throws Exception {
+        //given
+        LocalLoginRequestDto localLoginRequestDto =
+                LocalLoginRequestDto.builder()
+                        .email(email)
+                        .password(password)
+                        .build();
+        //when
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/api/auth/local/sign-in")
+                .content(objectMapper.writeValueAsString(localLoginRequestDto))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        //then
+        UserAuthResponse userAuthResponse = objectMapper.readValue((mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString()), UserAuthResponse.class );
+
+        //jwt token이 정상적인가
+        assertThat(jwtUtils.validateToken(userAuthResponse.getAccessToken())).isTrue();
+
+    }
 }
