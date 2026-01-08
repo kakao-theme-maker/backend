7fed24ee6036829e8409d3b53381d3ab3f768008
klaqwe1 <klaqwe1@naver.com>
2026-01-09 03:49:46 +0900

    feat: 유저 게시물 목록 조회 기능 구현 Related to: #39

--- src/main/java/com/komentum/post/service/PostService.java
+++ src/main/java/com/komentum/post/service/PostService.java
@@ -1,11 +1,14 @@
 package com.komentum.post.service;
 
 import com.komentum.global.exception.CustomEntityNotFoundException;
+import com.komentum.global.utils.FileManager;
 import com.komentum.post.domain.Post;
 import com.komentum.post.dto.PostDto.PostCreateDto;
 import com.komentum.post.dto.PostDto.PostUpdateDto;
+import com.komentum.post.dto.PostDto.UserPostListResponseDto;
 import com.komentum.post.repository.PostRepository;
 import com.komentum.user.domain.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +18,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostService {
 
   private final PostRepository postRepository;
+  private final FileManager fileManager;
 
   @Transactional(readOnly = true)
   public Post getPostByPostId(Long postId) {
     return postRepository.findById(postId)
         .orElseThrow(() -> new CustomEntityNotFoundException(Post.class, postId));
+  }
+
+  public List<Post> getPostsByUserEmail(String userEmail){
+    return postRepository.findByUser_UserEmail(userEmail);
   }
 
   @Transactional
@@ -41,6 +49,19 @@ public class PostService {
   public void deletePost(Long postId) {
     Post targetPost = getPostByPostId(postId);
     postRepository.deleteById(targetPost.getPostId());
+  }
+
+  // 유저가 작성한 게시글 목록 조회 메서드
+  public List<UserPostListResponseDto> findUserPostList(String userEmail){
+
+    // 필요한 정보 추출
+    return getPostsByUserEmail(userEmail).stream() // userEmail 로 Post 리스트 가져옴
+        .map(post -> UserPostListResponseDto.builder().postId(post.getPostId())
+            .previewImageUrl(fileManager.resolveFilePath(post.getPreviewImageName())) // url 가져오기
+                .createdAt(post.getCreatedAt())
+                .updatedAt(post.getUpdatedAt())
+                .build())
+        .toList(); // 리스트 변환
   }
 
   // 업로드 수 count 반환 메서드
