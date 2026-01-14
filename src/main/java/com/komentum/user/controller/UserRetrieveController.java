048a50fc4b4d1ba2f2dbbbd09472500f12324fbd
klaqwe1 <klaqwe1@naver.com>
2026-01-14 21:52:21 +0900

    refactor: change http method HTTP 메서드 변경, api 경로 변경 Related to: #35

--- src/main/java/com/komentum/user/controller/UserRetrieveController.java
+++ src/main/java/com/komentum/user/controller/UserRetrieveController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,10 +34,10 @@ public class UserRetrieveController {
   }
 
   // 유저 정보 조회
-  @PostMapping("/info")
-  public ResponseEntity<CustomResponse<UserResponseDto>> getUserByEmail(@RequestBody UserRequestDto requestDto) {
+  @GetMapping("")
+  public ResponseEntity<CustomResponse<UserResponseDto>> getUserByEmail(@RequestParam String userEmail) { // 대체키 추가 후에 수정
     try {
-      UserResponseDto user = userRetrieveService.getUserByEmail(requestDto.getUserEmail());
+      UserResponseDto user = userRetrieveService.getUserByEmail(userEmail);
       // 유저 조회 성공
       return ResponseEntity.ok(CustomResponse.ok(user));
     } catch (RuntimeException e){
@@ -47,7 +48,7 @@ public class UserRetrieveController {
   }
 
   // 유저 정보 수정
-  @PutMapping("/info")
+  @PatchMapping("/me")
   public ResponseEntity<CustomResponse<UserResponseDto>> updateUser(
       @RequestBody UserUpdateDto updateDto,
       @AuthenticationPrincipal CustomUserDetails userDetails){
@@ -59,7 +60,7 @@ public class UserRetrieveController {
   }
 
   // Local 비밀번호 변경 기능
-  @PatchMapping("/info")
+  @PatchMapping("/me/password")
   public ResponseEntity<String> changePassword(@RequestBody PasswordChangeRequsetDto passwordChangeRequsetDto,
       @AuthenticationPrincipal CustomUserDetails userDetails){
     userAuthService.changePassword(userDetails.getUsername(), passwordChangeRequsetDto);
