c86ddbb5547e58917ff991a75dbb4952d8cb5c94
kym8821 <louie9798@naver.com>
2026-01-20 20:29:24 +0900

    feat: [#34] CategoryPost 엔티티에 대한 DTO, facade, controller 레이어 추가
    - facade 계층에 게시글-카테고리 매핑 정보 등록 / 제거 기능 추가
    - CategoryPost에 대한 응답용 객체 추가
    - 게시글-카테고리 매핑 정보 관리를 위한 컨트롤러 추가
    
    Fixes: #34

---
+++ src/main/java/com/komentum/post/dto/CategoryPostDto.java
@@ -0,0 +1,32 @@
+package com.komentum.post.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.komentum.post.domain.CategoryPost;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class CategoryPostDto {
+
+  @Getter
+  @Setter
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class CategoryPostResponse {
+
+    @JsonProperty("category_id")
+    Long categoryId;
+    @JsonProperty("postId")
+    Long postId;
+
+    public static CategoryPostResponse from(CategoryPost categoryPost) {
+      return CategoryPostResponse.builder()
+          .categoryId(categoryPost.getCategory().getCategoryId())
+          .postId(categoryPost.getPost().getPostId())
+          .build();
+    }
+  }
+}
--- src/main/java/com/komentum/post/facade/CategoryPostManagementFacade.java
+++ src/main/java/com/komentum/post/facade/CategoryPostManagementFacade.java
@@ -1,10 +1,14 @@
 package com.komentum.post.facade;
 
+import com.komentum.post.domain.Category;
+import com.komentum.post.domain.Post;
+import com.komentum.post.dto.CategoryPostDto.CategoryPostResponse;
 import com.komentum.post.service.CategoryPostService;
 import com.komentum.post.service.CategoryService;
 import com.komentum.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +17,27 @@ public class CategoryPostManagementFacade {
   private final PostService postService;
   private final CategoryService categoryService;
   private final CategoryPostService categoryPostService;
+
+  /**
+   * 카테고리에 게시글이 있으면 유지, 없으면 추가
+   * @param categoryId 카테고리 ID
+   * @param postId 게시글 ID
+   * */
+  @Transactional
+  public CategoryPostResponse registerPostOnCategory(Long categoryId, Long postId) {
+    Category targetCategory = categoryService.findById(categoryId);
+    Post targetPost = postService.getPostByPostId(postId);
+    return CategoryPostResponse.from(
+        categoryPostService.registerCategoryPost(targetCategory, targetPost));
+  }
+
+  /**
+   * 카테고리에 있는 게시글 제거하고, 제거할 카테고리-게시글 정보가 없으면 예외 발생
+   * @param categoryId 게시글을 제외할 카테고리 식별자
+   * @param postId 카테고리에서 제외할 게시글 식별자
+   * */
+  @Transactional
+  public void deletePostFromCategory(Long categoryId, Long postId) {
+    categoryPostService.deleteCategoryPost(categoryId, postId);
+  }
 }
