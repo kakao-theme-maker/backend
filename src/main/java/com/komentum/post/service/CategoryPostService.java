1d0445e76446d2858ac1a3b0c204a7bdd90a25e0
kym8821 <louie9798@naver.com>
2026-01-20 20:27:13 +0900

    refactor: [#34] CategoryPost 도메인 서비스 가독성 개선
    - delete 메서드의 예외 객체 타입과 예외 메세지 개선
    - register 메서드 기능 확장 ( 데이터를 삽입하되, 기존 매핑 정보가 있다면 대신 기존 정보 반환 )
    
    Fixes: #34

--- src/main/java/com/komentum/post/service/CategoryPostService.java
+++ src/main/java/com/komentum/post/service/CategoryPostService.java
@@ -4,6 +4,7 @@ import com.komentum.post.domain.Category;
 import com.komentum.post.domain.CategoryPost;
 import com.komentum.post.domain.Post;
 import com.komentum.post.repository.CategoryPostRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +16,7 @@ public class CategoryPostService {
   private final CategoryPostRepository categoryPostRepository;
 
   /**
-   * 특정 게시글을 카테고리에 등록
+   * 특정 게시글을 카테고리에 등록하고, 기존 매핑 정보가 있다면 중복 없이 유지
    *
    * @param category 게시글이 등록되는 카테고리 Entity
    * @param post 카테고리에 등록할 게시글 Entity
@@ -23,10 +24,12 @@ public class CategoryPostService {
    * */
   @Transactional
   public CategoryPost registerCategoryPost(Category category, Post post) {
-    return categoryPostRepository.save(CategoryPost.builder()
-        .category(category)
-        .post(post)
-        .build());
+    return categoryPostRepository
+        .findByCategory_CategoryIdAndPost_PostId(category.getCategoryId(), post.getPostId())
+        .orElseGet(() -> categoryPostRepository.save(CategoryPost.builder()
+            .category(category)
+            .post(post)
+            .build()));
   }
 
   /**
@@ -37,9 +40,11 @@ public class CategoryPostService {
    * */
   @Transactional
   public void deleteCategoryPost(Long categoryId, Long postId) {
-    CategoryPost categoryPost = categoryPostRepository.findByCategory_CategoryIdAndPost_PostId(
-        categoryId, postId).orElseThrow(
-        () -> new RuntimeException("cannot find category-post by category and post id"));
-    categoryPostRepository.deleteById(categoryPost.getCategoryPostId());
+    CategoryPost target = categoryPostRepository
+        .findByCategory_CategoryIdAndPost_PostId(categoryId, postId)
+        .orElseThrow(
+            () -> new EntityNotFoundException(
+                "CategoryPost not found. categoryId=" + categoryId + ", postId=" + postId));
+    categoryPostRepository.deleteById(target.getCategoryPostId());
   }
 }
