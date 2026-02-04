d042df287a75f0b1c049dd748c6f7682a96d76a4
kym8821 <louie9798@naver.com>
2026-02-04 21:02:29 +0900

    fix: [#46 - ] CustomUserDetails 전역 Getter 제거 - 필드 접근 제한을 위해 전역적 gettter가 아닌 선택적으로 Getter를 사용하도록 수정 Fixes: #46

--- src/main/java/com/komentum/global/dto/CustomUserDetails.java
+++ src/main/java/com/komentum/global/dto/CustomUserDetails.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@Getter
 public class CustomUserDetails implements UserDetails {
 
   @Getter
