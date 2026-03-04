2611cb25ec59f94adb47235cf3d7d7bf877c2cef
kym8821 <louie9798@naver.com>
2026-03-04 16:29:03 +0900

    chore: [#51 -] 카테고리 API swagger 문서화 - CategoryController와 CategoryPostController API 설명 추가 - CategoryDto, CategoryPostDto @Schema 추가 Fixes: #51

--- src/main/java/com/komentum/post/controller/CategoryController.java
+++ src/main/java/com/komentum/post/controller/CategoryController.java
@@ -5,6 +5,7 @@ import com.komentum.post.dto.CategoryDto.CategoryCreateDto;
 import com.komentum.post.dto.CategoryDto.CategoryResponseDto;
 import com.komentum.post.dto.CategoryDto.CategoryUpdateDto;
 import com.komentum.post.facade.CategoryManagementFacade;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +31,7 @@ public class CategoryController {
    * 특정 사용자가 생성한 모든 카테고리 조회
    * */
   @GetMapping
+  @Operation(summary = "이메일=user_email인 사용자가 소유한 모든 카테고리를 조회한다")
   public ResponseEntity<List<CategoryResponseDto>> findAllByUser(
       @RequestParam("user_email") String userEmail) {
     return ResponseEntity.ok(categoryManagementFacade.findAllByUser(userEmail));
@@ -39,6 +41,7 @@ public class CategoryController {
    * 특정 사용자가 새로운 카테고리 생성
    * */
   @PostMapping
+  @Operation(summary = "현재 인증된 사용자가 카테고리를 생성한다")
   public ResponseEntity<CategoryResponseDto> saveCategory(
       @RequestBody CategoryCreateDto createDto,
       @AuthenticationPrincipal CustomUserDetails userDetails) {
@@ -50,6 +53,7 @@ public class CategoryController {
    * 특정 사용자가 생성한 카테고리 수정
    * */
   @PatchMapping("/{category_id}")
+  @Operation(summary = "현재 인증된 사용자가 자신이 소유한 ID=category_id인 카테고리를 수정한다")
   public ResponseEntity<CategoryResponseDto> updateCategory(
       @PathVariable("category_id") Long categoryId, @RequestBody CategoryUpdateDto updateDto,
       @AuthenticationPrincipal CustomUserDetails userDetails) {
@@ -61,6 +65,7 @@ public class CategoryController {
    * 특정 사용자가 생성한 카테고리 삭제
    * */
   @DeleteMapping("/{category_id}")
+  @Operation(summary = "현재 인증된 사용자가 자신이 소유한 ID=category_id인 카테고리를 삭제한다")
   public ResponseEntity<Void> deleteCategory(@PathVariable("category_id") Long categoryId,
       @AuthenticationPrincipal CustomUserDetails userDetails) {
     categoryManagementFacade.deleteCategory(categoryId, userDetails.getUsername());
