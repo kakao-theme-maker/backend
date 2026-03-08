1b6a2df4d97bc1cddcea71f527f848333e4e8d00
klaqwe1 <klaqwe1@naver.com>
2026-03-09 02:45:46 +0900

    fix: owner를 잘못 받고 있던 문제 수정 Related to: #52

--- src/main/java/com/komentum/theme/component/domain/policy/DesignComponentPolicy.java
+++ src/main/java/com/komentum/theme/component/domain/policy/DesignComponentPolicy.java
@@ -1,21 +1,22 @@
 package com.komentum.theme.component.domain.policy;
 
 import com.komentum.global.domain.policy.OwnerAdminPolicy;
-import com.komentum.user.domain.User;
+import com.komentum.theme.component.domain.DesignComponent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class DesignComponentPolicy {
+
   private final OwnerAdminPolicy ownerAdminPolicy;
 
-  public boolean canUpdate(User designComponentOwner) {
-    return ownerAdminPolicy.validate(designComponentOwner);
+  public boolean canUpdate(DesignComponent designComponent) {
+    return ownerAdminPolicy.validate(designComponent.getUser());
   }
 
-  public boolean canDelete(User designComponentOwner) {
-    return ownerAdminPolicy.validate(designComponentOwner);
+  public boolean canDelete(DesignComponent designComponent) {
+    return ownerAdminPolicy.validate(designComponent.getUser());
   }
 
 }
