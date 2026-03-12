f50d78d49086ccf4f65f4ac1e3502c2a6e0e3745
kym8821 <louie9798@naver.com>
2026-03-12 14:09:55 +0900

    fix: [#57 - ] devAuthController에서 매번 다른 사용자의 정보로 토큰을 생성하는 문제 수정 - 매번 같은 사용자 정보로 토큰을 생성하도록 변경 Fixes: #57

--- src/main/java/com/komentum/user/controller/DevAuthController.java
+++ src/main/java/com/komentum/user/controller/DevAuthController.java
@@ -10,9 +10,11 @@ import com.komentum.user.repository.UserRepository;
 import com.komentum.user.service.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import java.time.LocalDate;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,15 +28,18 @@ public class DevAuthController {
   private final UserRepository userRepository;
   private final TokenService tokenService;
   private final JwtUtils jwtUtils;
+  private final BCryptPasswordEncoder passwordEncoder;
 
   @PostMapping("/users/auth")
   @Operation(summary = "DB의 무작위 사용자를 기반으로 access token과 refresh token 발급")
   public ResponseEntity<UserAuthResponse> getTestUserAuth() {
-    User user = userRepository.findAll().get(0);
+    User user = userRepository.findByUserEmail("test@test.com").orElse(null);
     if (user == null) {
       Faker faker = new Faker();
       user = userRepository.save(User.builder()
           .userEmail("test@test.com")
+          .publicUserId(UUID.randomUUID().toString())
+          .encryptedPassword(passwordEncoder.encode("1234"))
           .role(UserRole.USER)
           .birth(LocalDate.now().minusYears(10))
           .gender(Gender.male)
