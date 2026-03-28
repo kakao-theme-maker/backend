6a0c63d3ecf348a5e629ea39feda5206be0423cd
kym8821 <louie9798@naver.com>
2026-03-28 16:19:48 +0900

    feat: [#55 - ] Color Style, Component Type API에 ADMIN 접근 권한 설정 - /api/color-styles/**와 /api/component-types/**에 ADMIN User만 접근하도록 설정 Fixes: #55

--- src/main/java/com/komentum/global/security/SecurityConfig.java
+++ src/main/java/com/komentum/global/security/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> {
           auth.requestMatchers(securityProperties.getWhiteList()).permitAll();
           auth.requestMatchers(HttpMethod.GET, securityProperties.getWhiteListGet()).permitAll();
+          auth.requestMatchers(new String[]{"/api/color-styles/**", "/api/component-types/**"})
+              .hasRole(UserRole.ADMIN.name());
           // 로컬 스토리지를 사용하는 경우 업로드된 파일을 정적 리소스로 직접 서빙하므로 업로드 경로에 대한 GET 요청을 허용
           if (fileStorageProperty.getStorage() == Storage.LOCAL) {
             auth.requestMatchers(HttpMethod.GET, WebConfig.UPLOAD_URL_PREFIX + "/**").permitAll();
