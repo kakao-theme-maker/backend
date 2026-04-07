09cc6ceece91d20eebe9c878c04b53c43f782c98
kym8821 <louie9798@naver.com>
2026-04-08 06:28:15 +0900

    feat: [ #72 - ] 파일 스토리지 관련 환경변수 관리 방식 변경 - @Valuse + @ConfigurationPropertise -> @ConfigurationProperties 단일 방식으로 변경 - base url에 대한 설정 정보 정상 적용 여부를 확인하기 위한 로그 추가 Fixes: #72

--- src/main/java/com/komentum/global/properties/FileStorageProperty.java
+++ src/main/java/com/komentum/global/properties/FileStorageProperty.java
@@ -1,13 +1,21 @@
 package com.komentum.global.properties;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Slf4j
 @Getter
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "file")
 public class FileStorageProperty {
+
+  @PostConstruct
+  public void init() {
+    log.info("FileStorage baseUrl={}", baseUrl);
+  }
 
   private final String baseUrl;
   private final Storage storage;
