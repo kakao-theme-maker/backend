73e41bb3b81c9512409444d917d8ea81ba9e4aba
klaqwe1 <klaqwe1@naver.com>
2026-04-08 18:44:08 +0900

    refactor: schema, operation 추가 theme manage, theme retireve 수정 Related to: #79

--- src/main/java/com/komentum/theme/theme/dto/ThemeImageRequest.java
+++ src/main/java/com/komentum/theme/theme/dto/ThemeImageRequest.java
@@ -1,5 +1,6 @@
 package com.komentum.theme.theme.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,8 +12,12 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "테마 이미지 요청용 DTO")
 public class ThemeImageRequest {
 
+  @Schema(description = "디자인 컴포넌트의 식별자", example = "1")
   Integer designComponentId;
+
+  @Schema(description = "컴포넌트 타입의 식별자", example = "1")
   Integer componentTypeId;
 }
