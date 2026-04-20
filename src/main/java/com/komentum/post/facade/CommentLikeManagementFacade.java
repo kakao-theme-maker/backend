513976f08afdd3b91a0164952cadb99e07c71218
klaqwe1 <klaqwe1@naver.com>
2026-04-21 04:43:25 +0900

    refactor: use userEntityFinder instead of userRetrieveService Related to: #87

--- src/main/java/com/komentum/post/facade/CommentLikeManagementFacade.java
+++ src/main/java/com/komentum/post/facade/CommentLikeManagementFacade.java
@@ -2,7 +2,7 @@ package com.komentum.post.facade;
 
 import com.komentum.post.service.CommentLikeService;
 import com.komentum.user.domain.User;
-import com.komentum.user.service.UserRetrieveService;
+import com.komentum.user.service.UserEntityFinder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +11,15 @@ import org.springframework.stereotype.Service;
 public class CommentLikeManagementFacade {
 
   private final CommentLikeService commentLikeService;
-  private final UserRetrieveService userRetrieveService;
+  private final UserEntityFinder userEntityFinder;
 
   public void like(Long commentId, String clientId) {
-    User targetUser = userRetrieveService.findUserEntity(clientId);
+    User targetUser = userEntityFinder.findUserEntity(clientId);
     commentLikeService.like(commentId, targetUser.getUserId());
   }
 
   public void unlike(Long commentId, String clientId) {
-    User targetUser = userRetrieveService.findUserEntity(clientId);
+    User targetUser = userEntityFinder.findUserEntity(clientId);
     commentLikeService.unlike(commentId, targetUser.getUserId());
   }
 }
