93de9d9a7afee6eea5de9db3512165463705530b
kym8821 <louie9798@naver.com>
2026-04-23 22:23:48 +0900

    chore: [#100 - ] ThemePreviewDto Swagger 문서 추거 - 클래스, 각 필드 설명 추가 Fixes: #100

--- src/main/java/com/komentum/theme/theme/dto/ThemePreviewDto.java
+++ src/main/java/com/komentum/theme/theme/dto/ThemePreviewDto.java
@@ -1,6 +1,7 @@
 package com.komentum.theme.theme.dto;
 
 import com.komentum.theme.theme.domain.ThemeComponent;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,12 +12,18 @@ import lombok.Setter;
 @Setter
 @Builder
 @AllArgsConstructor
+@Schema(description = "테마 미리보기 응답 DTO")
 public class ThemePreviewDto {
 
+  @Schema(description = "테마 식별자")
   private final Integer themeComponentId;
+  @Schema(description = "테마 대표 이미지")
   private final String previewImageUrl;
+  @Schema(description = "테마 이름")
   private final String themeName;
+  @Schema(description = "테마 생성일")
   private final LocalDateTime createdAt;
+  @Schema(description = "테마 갱신일")
   private final LocalDateTime updatedAt;
 
   public static ThemePreviewDto from(ThemeComponent themeComponent, String previewImageUrl) {
