fecc015d82131b3e30ae0542a4ecaeeca480b98d
kym8821 <louie9798@naver.com>
2026-05-12 07:29:22 +0900

    fix: [#105 - ] PlatformComponentType, ColorStyleUpdateRequest 문서 및 스키마 개선 - PlatformComponentType에 @JoinColumn 명시 - ColorStyleUpdateRequest에서 null이 가능한 필드에 대해 명시 Fixes: #105

--- src/main/java/com/komentum/theme/component/domain/PlatformComponentType.java
+++ src/main/java/com/komentum/theme/component/domain/PlatformComponentType.java
@@ -10,6 +10,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -47,5 +48,6 @@ public class PlatformComponentType {
   private Platform platform;
 
   @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "component_type_id")
   private ComponentType componentType;
 }
--- src/main/java/com/komentum/theme/component/dto/ColorStyleUpdateRequest.java
+++ src/main/java/com/komentum/theme/component/dto/ColorStyleUpdateRequest.java
@@ -17,9 +17,9 @@ public class ColorStyleUpdateRequest {
   @Schema(description = "수정할 color style 설명, null 허용")
   private String explain;
 
-  @Schema(description = "수정할 color style 이름")
+  @Schema(description = "수정할 color style 이름, null 허용")
   private String name;
 
-  @Schema(description = "수정할 color style 종류")
+  @Schema(description = "수정할 color style 종류, null 허용")
   private StyleCode styleCode;
 }
