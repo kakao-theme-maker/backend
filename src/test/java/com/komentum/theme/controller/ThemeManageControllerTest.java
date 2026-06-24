6933eb061766d5f90dfd2b2c61d350f5365ea0d9
kym8821 <louie9798@naver.com>
2026-06-24 16:11:58 +0900

    test: [#127 - ] 테마 수정 / 상세조회 테스트 개발 - 테마 수정 테스트 개발 및 테스트 데이터 생성 방식 변경 - 테마 상세 조회 테스트 개발 및 테스트 데이터 생성 방식 변경 Fixes: #127

--- src/test/java/com/komentum/theme/controller/ThemeManageControllerTest.java
+++ src/test/java/com/komentum/theme/controller/ThemeManageControllerTest.java
@@ -6,6 +6,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.komentum.designcomponent.domain.DesignComponent;
+import com.komentum.designcomponent.enums.StyleCode;
+import com.komentum.designcomponent.enums.TypeCode;
 import com.komentum.test.MockMvcUtils;
 import com.komentum.test.config.EnableTestProfile;
 import com.komentum.test.data.TestDataRemover;
@@ -16,9 +18,19 @@ import com.komentum.test.data.scenario.UserScenarioSupport;
 import com.komentum.test.dto.MockMvcRequestDto.ExecutionContext;
 import com.komentum.test.dto.TestClientDto;
 import com.komentum.theme.core.domain.ThemeComponent;
+import com.komentum.theme.core.domain.ThemeImage;
+import com.komentum.theme.core.domain.ThemeStyle;
 import com.komentum.theme.core.dto.ThemeDetailResponse;
+import com.komentum.theme.core.dto.ThemeUpdateRequest;
+import com.komentum.theme.core.dto.ThemeUpdateRequest.ThemeImageUpdateRequest;
+import com.komentum.theme.core.dto.ThemeUpdateRequest.ThemeStyleUpdateRequest;
+import com.komentum.theme.core.repository.ThemeComponentRepository;
+import com.komentum.theme.core.repository.ThemeImageRepository;
+import com.komentum.theme.core.repository.ThemeStyleRepository;
 import com.komentum.user.domain.User;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -30,6 +42,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @EnableTestProfile
@@ -56,6 +69,12 @@ class ThemeManageControllerTest {
   private DesignComponentScenarioSupport designComponentScenarioSupport;
   @Autowired
   private ThemeDataGenerator themeDataGenerator;
+  @Autowired
+  private ThemeImageRepository themeImageRepository;
+  @Autowired
+  private ThemeStyleRepository themeStyleRepository;
+  @Autowired
+  private ThemeComponentRepository themeComponentRepository;
 
   private User testUser;
   private List<DesignComponent> designComponentList;
@@ -76,7 +95,7 @@ class ThemeManageControllerTest {
   }
 
   @Test
-  @DisplayName("")
+  @DisplayName("새로운 테마 생성 시, 디폴트 테마를 복사해서 제공한다")
   public void createNewTheme_success() throws Exception {
     // given
     var themeResult = themeComponentScenarioSupport.builder(List.of(testUser), designComponentList)
@@ -101,34 +120,67 @@ class ThemeManageControllerTest {
   }
 
   @Test
-  @DisplayName("")
+  @DisplayName("사용자는 테마 이름, 이미지, 색상을 수정할 수 있다")
+  @Transactional
   public void updateTheme_success() throws Exception {
     // given
     ThemeComponent targetTheme = themeComponentScenarioSupport.builder(List.of(testUser),
             designComponentList)
         .withCountPerUser(1)
         .build().themeComponents().get(0);
-//    ThemeUpdateRequest request = ThemeUpdateRequest.builder()
-//        .themeName(UUID.randomUUID().toString())
-//        .styleCodes(Map.of(TypeCode.))
-//        .typeCodes(Map.of())
-//        .build();
-//    // when
-//    MockHttpServletRequestBuilder request = MockMvcRequestBuilders.put("/api/themes/{id}",
-//            target.getThemeComponentId())
-//        .contentType(MediaType.APPLICATION_JSON)
-//        .content(objectMapper.writeValueAsString(createThemeRequest));
-//    request = mockMvcUtils.addAuthentication(request, testUser.getPublicUserId());
-//    // then
-//    mockMvc.perform(request)
-//        .andExpect(status().isOk());
+    TypeCode updatedTypeCode = TypeCode.MAINVIEW_STYLE_PRIMARY_BACKGROUND_IMAGE;
+    StyleCode updatedStyleCode = StyleCode.CHAT_ROOM_BACKGROUND_COLOR;
+    String expectedThemeName = UUID.randomUUID().toString();
+    DesignComponent expectedImage = designComponentList.get(0);
+    String expectedColor = "#FFFFFF";
+    ThemeUpdateRequest updateRequestDto = ThemeUpdateRequest.builder()
+        .themeName(expectedThemeName)
+        .typeCodes(Map.of(
+            updatedTypeCode,
+            ThemeImageUpdateRequest.builder()
+                .designComponentId(expectedImage.getDesignComponentId())
+                .build()
+        ))
+        .styleCodes(Map.of(
+            updatedStyleCode,
+            ThemeStyleUpdateRequest.builder()
+                .color(expectedColor)
+                .build()
+        ))
+        .build();
+    // when
+    MockHttpServletRequestBuilder request = MockMvcRequestBuilders.put("/api/themes/{id}",
+        targetTheme.getThemeComponentId());
+    ResultActions resultActions = mockMvcUtils.performAuthRequest(request,
+        ExecutionContext.builder()
+            .mockMvc(mockMvc)
+            .body(updateRequestDto)
+            .clientDto(TestClientDto.fromEntity(testUser))
+            .build());
+    // then
+    resultActions.andExpect(status().isNoContent());
+    ThemeComponent updated = themeComponentRepository.findById(targetTheme.getThemeComponentId())
+        .orElseThrow();
+    ThemeImage updatedThemeImage = updated.getThemeImages().stream()
+        .filter(v -> v.getComponentType().getTypeCode().equals(updatedTypeCode))
+        .findFirst().orElseThrow();
+    ThemeStyle updatedThemeStyle = updated.getThemeStyles().stream()
+        .filter(v -> v.getColorStyle().getStyleCode().equals(updatedStyleCode))
+        .findFirst().orElseThrow();
+    assertThat(updated.getThemeName()).isEqualTo(expectedThemeName);
+    assertThat(updatedThemeImage.getDesignComponent().getDesignComponentId())
+        .isEqualTo(expectedImage.getDesignComponentId());
+    assertThat(updatedThemeStyle.getColor()).isEqualTo(expectedColor);
   }
 
   @Test
   @DisplayName("")
   public void markThemeAsDone_success() throws Exception {
     // given
-    ThemeComponent target = themeDataGenerator.initialThemes.get(0);
+    ThemeComponent target = themeComponentScenarioSupport.builder(List.of(testUser),
+            designComponentList)
+        .withCountPerUser(1)
+        .build().themeComponents().get(0);
     // when
     MockHttpServletRequestBuilder request = MockMvcRequestBuilders.put("/api/themes/{id}/done",
         target.getThemeComponentId());
@@ -142,7 +194,10 @@ class ThemeManageControllerTest {
   @DisplayName("")
   public void deleteTheme_success() throws Exception {
     // given
-    ThemeComponent target = themeDataGenerator.initialThemes.get(0);
+    ThemeComponent target = themeComponentScenarioSupport.builder(List.of(testUser),
+            designComponentList)
+        .withCountPerUser(1)
+        .build().themeComponents().get(0);
     // when
     MockHttpServletRequestBuilder request = MockMvcRequestBuilders.delete("/api/themes/{id}",
         target.getThemeComponentId());
