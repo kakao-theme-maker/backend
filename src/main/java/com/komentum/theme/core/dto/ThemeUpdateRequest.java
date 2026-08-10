c7a4c58bd514391e19e6bdb4e7e6fb3089c14c6c
kym8821 <louie9798@naver.com>
2026-08-10 16:39:52 +0900

    feat: [#151 - ] 테마 갱신 요청 DTO 수정 ( inset 정보 추가 ) Fixes: #151

--- src/main/java/com/komentum/theme/core/dto/ThemeUpdateRequest.java
+++ src/main/java/com/komentum/theme/core/dto/ThemeUpdateRequest.java
@@ -2,6 +2,7 @@ package com.komentum.theme.core.dto;
 
 import com.komentum.designcomponent.enums.StyleCode;
 import com.komentum.designcomponent.enums.TypeCode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,16 +15,40 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "테마 수정 요청 DTO")
 public class ThemeUpdateRequest {
+
+  @Schema(description = "변경할 테마의 이름")
+  String themeName;
+
+  @Schema(description = "변경할 이미지에 대한 typeCode:이미지 맵")
+  Map<TypeCode, ThemeImageUpdateRequest> typeCodes;
+
+  @Schema(description = "변경할 색상에 대한 styleCode:색상 맵")
+  Map<StyleCode, ThemeStyleUpdateRequest> styleCodes;
 
   @Getter
   @Setter
   @Builder
   @NoArgsConstructor
   @AllArgsConstructor
+  @Schema(description = "수정할 이미지 정보")
   public static class ThemeImageUpdateRequest {
 
     Integer designComponentId;
+    Inset inset;
+  }
+
+  @Setter
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class Inset {
+
+    int top;
+    int left;
+    int bottom;
+    int right;
   }
 
   @Getter
@@ -35,8 +60,4 @@ public class ThemeUpdateRequest {
 
     String color;
   }
-
-  String themeName;
-  Map<TypeCode, ThemeImageUpdateRequest> typeCodes;
-  Map<StyleCode, ThemeStyleUpdateRequest> styleCodes;
 }
